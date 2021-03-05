a88167fc83999831b20c969eddf46ec02d23c690
Rich Townsend <townsend@astro.wisc.edu>
2021-03-05 01:00:17 -0600

    Added proper setting of rpath

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,17 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
+import os
+
+extra_link_args=['-Wl,-rpath,{:s}/../build'.format(os.getcwd())]
+
 extensions = [
     Extension(
         'msg',
         ['msg/msg.pyx'],
         library_dirs=['../build'],
-        libraries=['cmsg']
+        libraries=['cmsg'],
+        extra_link_args=extra_link_args
         )
     ]
 
