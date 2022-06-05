7c2c317fec7f0fe0fbc534741772ec92d92d453e
Rich Townsend <townsend@astro.wisc.edu>
2022-06-05 15:25:36 -0500

    Fixed link to tarball

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,7 +111,7 @@ extlinks = {
 # Set site-wide targets
 
 targets = {
-    'tarball': 'https://github.com/rhdtownsend/msg/releases/download/v1.0-rc2/msg-1.0.tar.gz',
+    'tarball': 'https://github.com/rhdtownsend/msg/releases/download/v1.0/msg-1.0.tar.gz',
     'mesa-sdk': 'http://www.astro.wisc.edu/~townsend/static.php?ref=mesasdk',
     'mad-sdk': 'http://www.astro.wisc.edu/~townsend/static.php?ref=madsdk'
 }
