4595d224c39bc25cc313e74f2eb1c8a072dddee9
Rich Townsend <townsend@astro.wisc.edu>
2023-07-15 11:02:57 -0500

    Bumped version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,8 @@ import sphinx_rtd_theme
 
 project = 'MSG'
 author = 'Rich Townsend & The MSG Team'
-version = '1.2'
-branch = 'release-1.2'
+version = '1.3'
+branch = 'release-1.3'
 copyright = '2023, Rich Townsend & The MSG Team'
 
 # -- General configuration ---------------------------------------------------
