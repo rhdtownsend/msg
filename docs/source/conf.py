e2ab0cca213eb86b98457ad93eaf5f555f5bab39
Rich Townsend <townsend@astro.wisc.edu>
2026-03-02 16:04:20 -0600

    Reset to dev versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ import sphinx_rtd_theme
 
 project = 'MSG'
 author = 'Rich Townsend & The MSG Team'
-version = '2.1.1'
+version = '2.1.1+dev'
 release = version
-branch = 'release-2.1.1'
+branch = 'main'
 copyright = '2021-2026, Rich Townsend & The MSG Team'
 
 
